f4949b5e709d2a5780ffaae9634a60fe1087d702
Boytcho Teofilov <boytchoteofilov@users.noreply.github.com>
2021-02-10 12:40:33 +0000

    Navigation items change.

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -5,7 +5,6 @@
     using System.Diagnostics;
     using System.Linq;
     using System.Threading.Tasks;
-
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
     using Web.Models;
