963c9937d97580dc185d79ecadbbe4813981d717
Boytcho Teofilov <boyateo@gmail.com>
2021-02-12 11:55:16 +0000

    ShortcutEntityModified

--- src/Infrastructure/Migrations/20210212115252_DeletedShotcutEntityPropertyCommand.Designer.cs
+++ src/Infrastructure/Migrations/20210212115252_DeletedShotcutEntityPropertyCommand.Designer.cs
@@ -4,14 +4,16 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(EfMSQLContext))]
-    partial class EfDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210212115252_DeletedShotcutEntityPropertyCommand")]
+    partial class DeletedShotcutEntityPropertyCommand
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Infrastructure/Migrations/20210212115252_DeletedShotcutEntityPropertyCommand.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class DeletedShotcutEntityPropertyCommand : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Command",
+                table: "Shortcuts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Command",
+                table: "Shortcuts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
